a702053eb302bfcfdc20573eeaadcb2d8281c03f
Yogesh Kumar Sharma <yogeshs1649@gmail.com>
2025-01-30 09:00:18 +0530

    Initial commit

---
+++ NumIsEvenOrNot.py
@@ -0,0 +1,10 @@
+# Program to check if a number is even or odd
+
+# Taking user input
+num = int(input("Enter a number: "))
+
+# Checking if the number is even or odd
+if num % 2 == 0:
+    print(f"{num} is an even number.")
+else:
+    print(f"{num} is an odd number.")
